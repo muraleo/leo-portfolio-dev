15896a89988b37dc91b661abfcbca2b3819b692b
muraleo <muraleo80923@gmail.com>
2018-11-19 12:41:57 -0500

    make text in skilllabel in the center

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -2,13 +2,36 @@ import React, { Component } from "react";
 import classes from "./projects.module.css";
 import Project from "./Project/project";
 import Skills from "./Skills/skills";
-import { Container, Row, Col } from "reactstrap";
+import {
+	Container,
+	Row,
+	Col,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter
+} from "reactstrap";
 import BurgerBuilderImage from "../../assets/images/burger_builder-min.png";
 import Omnifood from "../../assets/images/omnifood-min.png";
 import EwLandingPage from "../../assets/images/ew_landing_page-min.png";
 import EwCheckout from "../../assets/images/ew_checkout_1-min.png";
 
 class Projects extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState({
+			modal: !this.state.modal
+		});
+	}
 	render() {
 		return (
 			<div id="projects" className={classes.projects}>
@@ -85,11 +108,41 @@ class Projects extends Component {
 								<Skills
 									skill_list={["Rails", "SASS", "Javascript"]}
 								/>
-								<a>View Screenshots</a>
+								<a onClick={this.toggle}>View Screenshots</a>
 							</Project>
 						</Col>
 					</Row>
 				</Container>
+				<div>
+					<Modal
+						isOpen={this.state.modal}
+						toggle={this.toggle}
+						className={this.props.className}
+					>
+						<ModalHeader toggle={this.toggle}>
+							Modal title
+						</ModalHeader>
+						<ModalBody>
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore
+							eu fugiat nulla pariatur. Excepteur sint occaecat
+							cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</ModalBody>
+						<ModalFooter>
+							<Button color="primary" onClick={this.toggle}>
+								Do Something
+							</Button>{" "}
+							<Button color="secondary" onClick={this.toggle}>
+								Cancel
+							</Button>
+						</ModalFooter>
+					</Modal>
+				</div>
 			</div>
 		);
 	}
