aba416a0c1336fee5c67aae1102f4d046cf8d506
muraleo <muraleo80923@gmail.com>
2018-11-19 16:33:50 -0500

    Updates

---
+++ precache-manifest.5cf9a957a38b48b45e1f5039460e9fae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c125d93fd71a15faaa6b8c1f14d7bdb",
+    "url": "/leo-portfolio/static/media/burger_builder-min.8c125d93.png"
+  },
+  {
+    "revision": "7fbc814626a0210e1e18",
+    "url": "/leo-portfolio/static/css/main.5a7b97a8.chunk.css"
+  },
+  {
+    "revision": "3375aae0c03df248b32a00aef15fca3e",
+    "url": "/leo-portfolio/static/media/ew_checkout_4-min.3375aae0.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/leo-portfolio/static/js/1.343cbfa5.chunk.js"
+  },
+  {
+    "revision": "c7a8de31f916d1e670b1",
+    "url": "/leo-portfolio/static/js/runtime~main.c7a8de31.js"
+  },
+  {
+    "revision": "73cde5f619913be31db279f66dd93f65",
+    "url": "/leo-portfolio/static/media/ew_checkout_1-min.73cde5f6.png"
+  },
+  {
+    "revision": "ddd158cc031317b1f170c8096902ba7e",
+    "url": "/leo-portfolio/static/media/home-background-min.ddd158cc.jpg"
+  },
+  {
+    "revision": "7fbc814626a0210e1e18",
+    "url": "/leo-portfolio/static/js/main.7fbc8146.chunk.js"
+  },
+  {
+    "revision": "db0c5592a5e2b9213a9c05921998b88f",
+    "url": "/leo-portfolio/static/media/omnifood-min.db0c5592.png"
+  },
+  {
+    "revision": "24834250822aa84f2e16ca873b8ccdbe",
+    "url": "/leo-portfolio/static/media/ew_landing_page-min.24834250.png"
+  },
+  {
+    "revision": "702c96faad1bf76a5a61be951bc08437",
+    "url": "/leo-portfolio/static/media/ew_login-min.702c96fa.png"
+  },
+  {
+    "revision": "24a8556f808e2d8f78fea5f46fbbdd26",
+    "url": "/leo-portfolio/static/media/ew_checkout_2-min.24a8556f.png"
+  },
+  {
+    "revision": "45594dbfefdb97fda68f5dabb834e87c",
+    "url": "/leo-portfolio/static/media/ew_checkout_3-min.45594dbf.png"
+  },
+  {
+    "revision": "343cbfa5d57d450fd80b",
+    "url": "/leo-portfolio/static/css/1.2d641e84.chunk.css"
+  },
+  {
+    "revision": "2ee2eae917a89bf03fdeefcfa0adfa70",
+    "url": "/leo-portfolio/index.html"
+  }
+];
